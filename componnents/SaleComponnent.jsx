2817ad495448f0fad97eb79c72a373c25614244c
martu <martin.correa@redb.ee>
2024-03-10 15:12:19 -0300

    cosa loca

--- componnents/SaleComponnent.jsx
+++ componnents/SaleComponnent.jsx
@@ -73,7 +73,7 @@ const Sale = ({isServiceSelected, toggleModal, setSelectedServices, setServices,
 
 
     return (
-        <div style={styles.container}>
+        <div >
             {
                 isServiceSelected ?
                     (
@@ -125,7 +125,7 @@ const styles = {
         flexDirection: 'column',
         width: '100%',
         height: '100%',
-        minHeight: 100
+        minHeight: 400
     },
 
     containerServices: {
@@ -135,7 +135,7 @@ const styles = {
         flexDirection: 'column',
         width: '90%',
         height: '90%',
-        minHeight: 100
+        minHeight: 400
     },
     text: {
         fontFamily: 'Arial, Helvetica, sans-serif',
